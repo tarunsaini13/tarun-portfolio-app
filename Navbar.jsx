d7cb40db32debebc673cca0b95817c23b4b130be
satyamCubastion <Satyam.Singh@Cubastion.com>
2024-07-18 18:22:27 +0530

    init

---
+++ Navbar.jsx
@@ -0,0 +1,29 @@
+function Navbar(){
+    return (
+        <>
+            <div className="navbarWrapper">
+                <div className="title">
+                    <span>
+                        /
+                    </span>
+                </div>
+                <div className="title">
+                    <span>
+                        Work
+                    </span>
+                </div>
+                <div className="title">
+                    <span>
+                        Project
+                    </span>
+                </div>
+            </div>
+            <div className="cv-wrapper">
+                <span>
+                    CV
+                </span>
+            </div>
+        </>
+    )
+}
+export default Navbar;
