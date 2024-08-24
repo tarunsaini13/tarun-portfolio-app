6cdc40f4eddd4f1d458145a9821521f6beb73d4a
binaryWar <ashwk03@gmail.com>
2024-08-24 20:36:38 +0530

    added exp section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,17 +1,19 @@
-function Info(){
+function Info() {
+    const expDifference = new Date() - new Date("2021-12-01T00:00:00+05:30");
+    const expNum = expDifference / (365 * 24 * 60 * 60 * 1000);
     return (
         <section className="info-section">
             <div className="intro-wrapper">
                 <h1 className="welcome-text">Welcome</h1>
                 <p className="about-me">
-                    Hey,I'm Satyam. I'm a full stack developer based in India since 2022. I also love to code and do things for the web.
+                Hi, I'm Satyam, a Full Stack Developer with over {expNum.toFixed(1)} years of experience based in India. I am passionate about crafting innovative web solutions and coding dynamic applications that make an impact.
                 </p>
                 <div className="social-media-wrapper">
                     <div>
-                        <img src="src/assets/img/github.svg" alt=""/>
+                        <img src="src/assets/img/github.svg" alt="" />
                     </div>
                     <div>
-                        <img src="src/assets/img/mail.svg" alt="mail-image"/>
+                        <img src="src/assets/img/mail.svg" alt="mail-image" />
                     </div>
                 </div>
             </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,42 +2,80 @@ function Project() {
     const experineces = [
         {
             company_name: "Cubastion Consulting",
-            location: "Gurgaon|India",
+            location: "Gurgaon,India",
+            duration: "jan-2021 - jan 2022",
+            descriptions : [
+                "Built a microservices driven digital platform that simplifies the lending and borrowing of digital assets for institutions, improves tracking of financial transactions, and manages digital asset liquidity",
+                "Developed an onboarding module and backend RESTful API for seamless RFQ creation, onboarding institution, enhancing real-time bidding for RFQ",
+                "Integrated Copper third-party payment Gateway for loan fee payment",
+                "Implemented AWS Key Management Service (KMS) encryption and decryption",
+                "Reduced loan listing API loading time by 30%, managed concurrent requests to prevent timeouts and and eliminated multiple transaction histories using lock mechanisms."
+            ],
+            tech_stack: [
+                "Angular16","Node.js", "PostgresSQL", "Express.js", "HTML", "CSS", "Kafka"
+            ]
+        },
+        {
+            company_name: "Cubastion Consulting",
+            location: "Gurgaon,India",
+            duration: "jan-2021 - jan 2022",
+            descriptions : [
+                "this is first point lorem23",
+                "this is second point",
+                "this is third point"
+            ],
+            tech_stack: [
+                "Angular 16",
+                "Angular 16",
+                "Angular 16",
+            ]
+        },
+        {
+            company_name: "Cubastion Consulting",
+            location: "Gurgaon,India",
             duration: "jan-2021 - jan 2022",
             project_name: "HRMS Portal",
-            description: "Serviced based architecture where we are maintainig the complete cycle of employee.",
+            descriptions : [
+                "this is first point lorem23",
+                "this is second point",
+                "this is third point"
+            ],
             tech_stack: [
-                { image: "src/assets/img/angular.svg", title: "Angular 16" },
-                { image: "src/assets/img/angular.svg", title: "Angular 16" },
-                { image: "src/assets/img/angular.svg", title: "Angular 16" },
+                "Angular 16",
+                "Angular 16",
+                "Angular 16",
             ]
         }
     ]
     return (
         <section>
             <div className="skill-header">
-                <h1>Code : Upskill</h1>
+                <h1>Code : Work</h1>
             </div>
             <div className="project-wrapper">
                 {
                     experineces.map((exp, ind) => (
                         <div className="card">
                             <div className="timeline">
-                                <div className="company-title">{exp.company_name}{exp.location ? ',' + exp.location : ""}</div>
-                                <div className="time-period">{exp.duration}</div>
+                                <div className="location-period"><span>{exp.location ? exp.location : ""}</span> | {exp.duration}</div>
+                                <div className="company-name">{exp.company_name}</div>
                             </div>
-                            <h2 className="project-title">{exp.project_name}</h2>
+                            <span className="position-title">Full Stack Developer </span>
+                            {/* <h2 className="project-title">{exp.project_name}</h2> */}
                             <div className="bullet-point">
-                                {exp.description}
+                                {
+                                    exp.descriptions.map((e,k)=>(
+                                        <li key={ind+k}> {e} </li>
+                                    ))
+                                }
                             </div>
                             <div className="tech-stack-used">
-                                skills : 
+                                Tech Used : &nbsp;
                                 {
-                                    exp.tech_stack.map((tech, j) => (
-                                        <div className="chip">
-                                            <img src={tech.image} alt=".." height="25px" width="60px" />
-                                            <span>{tech.title}</span>
-                                        </div>
+                                    exp.tech_stack.map((s, j) => (
+                                        <span key={ind+j}>{s}
+                                        {j < exp.tech_stack.length-1 ? ', ' : '' }
+                                        </span>
                                     ))
                                 }
                             </div>
