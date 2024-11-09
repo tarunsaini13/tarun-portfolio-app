d8127298a7ad11826e3382fd5108861bc9cfcdd0
binaryWar <ashwk03@gmail.com>
2024-11-10 00:14:18 +0530

    modified the css and add scrollable fun

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import Info from './components/Info'
 import Navbar from './components/Navbar'
 import Skill from './components/Skill'
 import Project from './components/Project'
+import { useRef } from 'react'
 function App() {
-
+  const homeRef = useRef(null);
+  const skillRef = useRef(null);
+  const projectRef = useRef(null);
+  
   return (
     <>
-     <Navbar/>
-     <Info/>
-     <Skill/>
-     <Project/>
+      <Navbar scrollToHome={() => homeRef.current.scrollIntoView({ behavior: 'auto', block: "start"})}
+        scrollToSkill={() => skillRef.current.scrollIntoView({ behavior: 'auto',block:"start"})}
+        scrollToProject={() => projectRef.current.scrollIntoView({ behavior: 'auto',block:"start" })}
+      />
+      <div className="main-container">
+        <div className='section-wrapper' ref={homeRef}>
+          <Info/>
+        </div>
+        <div className='section-wrapper' ref={skillRef}>
+          <Skill/>
+        </div>
+        <div className='section-wrapper' ref={projectRef}>
+          <Project/>
+        </div>
+     </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
-function Navbar(){
+import { useState } from "react";
+
+function Navbar({scrollToProject,scrollToHome,scrollToSkill}){
+    const [activeHeader,setActiveHeader] = useState('home');
+    const onHeaderClick = (tab)=>{
+        console.log("running on header click")
+        setActiveHeader(tab);
+        if(tab === 'exp'){
+            scrollToProject();
+        }else if(tab === 'skill'){
+            scrollToSkill();
+        }else{
+            scrollToHome();
+        }
+    }
     return (
         <>
             <div className="navbarWrapper">
-                <div className="title">
+                <div className={`title ${activeHeader === 'home' ? 'active' : ''}`} onClick={()=>onHeaderClick('home')}>
                     <span>
                         /
                     </span>
                 </div>
-                <div className="title">
+                <div className={`title ${activeHeader === 'skill' ? 'active' : ''}`}  onClick={()=>onHeaderClick('skill')}>
                     <span>
-                        Work
+                        skill
                     </span>
                 </div>
-                <div className="title">
+                <div className={`title ${activeHeader === 'exp' ? 'active' : ''}`} onClick={()=>onHeaderClick('exp')}>
                     <span>
-                        Project
+                        Work
                     </span>
                 </div>
             </div>
