2613df18670455ea4fff4905152a7d1f9defd398
binaryWar <ashwk03@gmail.com>
2024-11-10 10:47:36 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Info from './components/Info'
 import Navbar from './components/Navbar'
 import Skill from './components/Skill'
 import Project from './components/Project'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
+
+
 function App() {
   const homeRef = useRef(null);
   const skillRef = useRef(null);
   const projectRef = useRef(null);
-  
+
   return (
     <>
-      <Navbar scrollToHome={() => homeRef.current.scrollIntoView({ behavior: 'auto', block: "start"})}
-        scrollToSkill={() => skillRef.current.scrollIntoView({ behavior: 'auto',block:"start"})}
-        scrollToProject={() => projectRef.current.scrollIntoView({ behavior: 'auto',block:"start" })}
+      <Navbar
+        homeRef={homeRef} 
+        skillRef={skillRef} 
+        projectRef={projectRef}
       />
-      <div className="main-container">
+      <div id='main-container'>
         <div className='section-wrapper' ref={homeRef}>
           <Info/>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function Navbar({scrollToProject,scrollToHome,scrollToSkill}){
-    const [activeHeader,setActiveHeader] = useState('home');
-    const onHeaderClick = (tab)=>{
-        console.log("running on header click")
+function Navbar({ homeRef, skillRef, projectRef }) {
+    const [activeHeader, setActiveHeader] = useState('home');
+    const onHeaderClick = (tab) => {
         setActiveHeader(tab);
-        if(tab === 'exp'){
+        if (tab === 'exp') {
             scrollToProject();
-        }else if(tab === 'skill'){
+        } else if (tab === 'skill') {
             scrollToSkill();
-        }else{
+        } else {
             scrollToHome();
         }
     }
+    const scrollToHome =() => homeRef.current.scrollIntoView({ behavior: 'auto', block: "start"});
+    const scrollToSkill = () => skillRef.current.scrollIntoView({ behavior: 'auto',block:"start"})
+    const scrollToProject=() => projectRef.current.scrollIntoView({ behavior: 'auto',block:"start" })
+    
+    const handleScroll = () => {
+        if (!homeRef.current || !skillRef.current || !projectRef.current) return;
+      
+        const homePosition = homeRef.current.getBoundingClientRect().top;
+        const skillPosition = skillRef.current.getBoundingClientRect().top;
+        const projectPosition = projectRef.current.getBoundingClientRect().top;
+      
+        if (homePosition <= 70 && skillPosition > 70) {
+            setActiveHeader('home');
+        } else if (skillPosition <= 70 && projectPosition > 70) {
+            setActiveHeader('skill');
+        } else if (projectPosition <= 70) {
+            setActiveHeader('exp');
+        }
+    };
+      
+
+    useEffect(() => {
+        if (homeRef.current && skillRef.current && projectRef.current) {
+            document.getElementById("main-container").addEventListener("scroll",handleScroll)
+        }
+        return ()=>{
+            document.getElementById("main-container").removeEventListener("scroll",handleScroll)
+        };
+    }, []); // Empty dependency array ensures it runs once after mount
+      
+
     return (
         <>
             <div className="navbarWrapper">
-                <div className={`title ${activeHeader === 'home' ? 'active' : ''}`} onClick={()=>onHeaderClick('home')}>
+                <div className={`title ${activeHeader === 'home' ? 'active' : ''}`} onClick={() => onHeaderClick('home')}>
                     <span>
                         /
                     </span>
                 </div>
-                <div className={`title ${activeHeader === 'skill' ? 'active' : ''}`}  onClick={()=>onHeaderClick('skill')}>
+                <div className={`title ${activeHeader === 'skill' ? 'active' : ''}`} onClick={() => onHeaderClick('skill')}>
                     <span>
                         skill
                     </span>
                 </div>
-                <div className={`title ${activeHeader === 'exp' ? 'active' : ''}`} onClick={()=>onHeaderClick('exp')}>
+                <div className={`title ${activeHeader === 'exp' ? 'active' : ''}`} onClick={() => onHeaderClick('exp')}>
                     <span>
                         Work
                     </span>
